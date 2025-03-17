8dcd5f66fbd84e75681c9baa6459c2b183f326b3
Denis Usmanov <info@chelnysite.ru>
2025-03-17 19:56:12 +0300

    Update message_handler.php

--- karmabot/message_handler.php
+++ karmabot/message_handler.php
@@ -8,10 +8,43 @@ function handleMessage($message) {
     $replyToMessage = isset($message['reply_to_message']) ? $message['reply_to_message'] : null;
 
     if (!in_array($chatId, $allowedChatIds)) {
-        $modx->log(modX::LOG_LEVEL_INFO, "Message received in non-allowed chat: $chatId");
+        $modx->log(modX::LOG_LEVEL_INFO, "Получено сообщение из не разрешённого чата: $chatId");
         return;
     }
 
+    // Обработка запросов к ИИ
+    $callAI = [
+        'ИИ ответь тоже',
+        'ИИ тоже ответь',
+        'тоже ответь ИИ',
+        'ответь тоже ИИ',
+        'ответь ИИ тоже'
+    ];
+
+    if (array_filter($callAI, function($phrase) use ($text) {
+        return mb_stripos($text, $phrase) !== false;
+    })) {
+        $text = $text . "Вопрос связан с MODX Revolution, ответь кратко и ёмко";
+        $response = getAIResponse($text);
+
+        // Логируем запрос и ответ
+        $modx->log(1, "Запрос: $question");
+        $modx->log(1, "Ответ: $response");
+
+        $cleanResponse = str_replace(array("\\boxed{", "}"), "", $response);
+
+        // Отправляем ответ в чат
+        $responseData = [
+            'chat_id' => $chatId,
+            'text' => $cleanResponse,
+        ];
+
+        $apiUrl = "https://api.telegram.org/bot$botToken/sendMessage?" . http_build_query($responseData);
+        file_get_contents($apiUrl);
+        return;
+    }
+
+    // Фильтрация фраз спамеров
     if (checkForBannedPhrases($text, $filters)) {
         $userId = $message['from']['id'];
         $username = isset($message['from']['username']) ? $message['from']['username'] : 'пользователь';
@@ -25,7 +58,7 @@ function handleMessage($message) {
             file_get_contents($deleteUrl);
 
             sendBanMessageAndDelete($chatId, $userId, $username, $botToken);
-            $modx->log(1, "Забанен Premium-пользователь @$username: $text");
+            $modx->log(1, "Забанен пользователь @$username: $text");
         } else {
             $deleteUrl = "https://api.telegram.org/bot$botToken/deleteMessage?chat_id=$chatId&message_id=" . $message['message_id'];
             file_get_contents($deleteUrl);
